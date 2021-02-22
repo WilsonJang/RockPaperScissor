525b1b3cf5099f0c0608c99b2e6912bc155b9205
Wilson <Wilsonjang10@gmail.com>
2021-02-22 13:54:49 -0500

    added images(will be replaced with better ones) and created some java script to show update on score and what the computer and player chose. Also made it so the code wont loop until a player makes a choice. Minor bugs where play wins/computer wins does not dissapear

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ let player = 0;
 let computer = 0;
 
 
+
+function rock() {
+    playerSelection="rock";
+    game();
+
+}
+function paper() {
+    playerSelection="paper";
+    game();
+}
+
+function scissor() {
+    playerSelection="scissor";
+    game();
+}
+
+
+
+//this function chooses random selection to be computers hand
 function computerPlay() {
     const hands = ["rock", "paper", "scissor"];
 
@@ -12,54 +31,63 @@ return hands[random]
 }
 
 
+//determines winner
 function playRound(playerSelection, computerSelection) {
     console.log("computer chose: " ,computerSelection)
     console.log("player chose: ", playerSelection)
     if (playerSelection==computerSelection) {
-        return "tie";
+        document.getElementById("computerChose").innerHTML = computerSelection;
+        document.getElementById("playerChose").innerHTML = playerSelection;
+        document.getElementById("tie").innerHTML = "Its a tie!";
     } else if (
         (computerSelection == "rock" && playerSelection == "scissor") ||
         (computerSelection == "scissor" && playerSelection == "paper") ||
         (computerSelection == "paper" && playerSelection == "rock")
       ) {
         computer++;
-        return "Computer wins";
+        document.getElementById("computerChose").innerHTML = computerSelection;
+        document.getElementById("playerChose").innerHTML = playerSelection;
+        document.getElementById("computerWins").innerHTML = "Computer wins!";
     
         } else {
            player++;
-        return "player wins";
-
+        document.getElementById("computerChose").innerHTML = computerSelection;
+        document.getElementById("playerChose").innerHTML = playerSelection;
+        document.getElementById("playerWins").innerHTML = "Player wins!";
         } 
                
     }
 
 function game() {
-    while (player < 5 && computer < 5){
-        playRound(playerSelection,computerSelection);
-        computerSelection=computerPlay();
+    if (player>=5 || computer>=5) {
+        return;
+    }
+    
+        playRound(playerSelection,computerSelection); //re runs the game
+        document.getElementById("playerScore").innerHTML = player;
+        document.getElementById("computerScore").innerHTML = computer;
+        computerSelection=computerPlay(); //chooses new hand
         console.log("playerScore: ",player)
         console.log("computerScore: ",computer)
-    }
+
     
       //console.log(playRound(playerSelection,computerSelection))
-    
-
 
     if (player == 5){
-        return "winner"
-
+        console.log( "winner"   )   
         
-        
-    
     }else if (computer == 5){
-        return "loser"
+        console.log( "winner"   ) 
+        
     }
 }
+function playagain() { //for future play again
+    player = 0;
+    computer = 0;
+    computerSelection=computerPlay();
+    playerSelection="";
+}
 
-
-
-
-const playerSelection = "rock";
+let playerSelection = "";
 let computerSelection = computerPlay();
 //console.log(playRound(playerSelection, computerSelection))
-console.log (game())
